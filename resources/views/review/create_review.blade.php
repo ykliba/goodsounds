f6e9d41e6386ebe48feee99878d6806613da69e5
ykliba <mediaboxing2@gmail.com>
2020-10-12 15:14:04 +0900

    マイグレーション

---
+++ resources/views/review/create_review.blade.php
@@ -0,0 +1,5 @@
+<x-app-layout>
+<x-slot name="header">
+</x-slot>
+  <h1>こんにちは</h1>
+</x-app-layout>
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,5 +13,8 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js')
     .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+        require('postcss-import'),
+        require('tailwindcss'),
+    ])
+    .sass('resources/sass/app.scss', 'public/css');
+    
